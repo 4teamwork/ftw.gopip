219e420534945ce4d3c79fb954b32c253020228c
Jonas Baumann <jone@jone.ch>
2016-09-25 19:32:02 +0200

    cleanup (pep8).

--- ftw/gopip/subscribers.py
+++ ftw/gopip/subscribers.py
@@ -4,7 +4,7 @@ from plone import api
 def root_page_order_changed(portal, event):
     catalog = api.portal.get_tool('portal_catalog')
     for brain in catalog.unrestrictedSearchResults({
-            'path': {'query':'/'.join(portal.getPhysicalPath()),
+            'path': {'query': '/'.join(portal.getPhysicalPath()),
                      'depth': 1}}):
         try:
             obj = brain.getObject()
